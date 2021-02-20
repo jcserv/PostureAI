183b311b7abc14ab3c8bdf80052de0205860e7d1
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-02-20 01:01:16 -0500

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,81 @@
-import { ChakraProvider, VStack } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import Webcam from 'react-webcam';
+import { ChakraProvider, VStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Webcam from "react-webcam";
 
-import { Footer } from './components/Footer';
-import { Form } from './components/Form';
-import { Header } from './components/Header';
-import { InfoBox } from './components/InfoBox';
-import { Navbar } from './components/Navbar';
+import { Footer } from "./components/Footer";
+import { Form } from "./components/Form";
+import { Header } from "./components/Header";
+import { InfoBox } from "./components/InfoBox";
+import { Navbar } from "./components/Navbar";
 
-import './App.css';
+import "./App.css";
 
 interface PageWrapperProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const PageWrapper: React.FC<PageWrapperProps> = ({ children }: PageWrapperProps) => {
-	return (
-		<div style={{ overflowX: 'hidden' }}>
-			<Navbar />
-			{children}
-			<Footer />
-		</div>
-	);
+const PageWrapper: React.FC<PageWrapperProps> = ({
+  children,
+}: PageWrapperProps) => {
+  return (
+    <div style={{ overflowX: "hidden" }}>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 function App() {
-	// one pixel image url xd
-	const [ imgSrc, setImgSrc ] = useState('https://i.imgur.com/AnRSQSq.png');
-	const webcamRef = React.useRef(null);
-	const capture = React.useCallback(
-		() => {
-			const ref = webcamRef.current as any;
-			const imageSrc = ref.getScreenshot();
-			setImgSrc(imageSrc);
-			/*
+  // one pixel image url xd
+  const [imgSrc, setImgSrc] = useState("https://i.imgur.com/AnRSQSq.png");
+  const webcamRef = React.useRef(null);
+  const capture = React.useCallback(() => {
+    const ref = webcamRef.current as any;
+    const imageSrc = ref.getScreenshot();
+    setImgSrc(imageSrc);
+    /*
 			console.log(
 				await testFunction(
 					document.getElementById("capture") as HTMLImageElement
 				)
 			)
 			*/
-		},
-		[ webcamRef ]
-	);
+  }, [webcamRef]);
 
-	useEffect(() => {
-		//loadModels();
-	}, []);
+  useEffect(() => {
+    //loadModels();
+  }, []);
 
-	return (
-		<div className="container">
-			<VStack className="column">
-				<Header />
-				<InfoBox />
-				<Webcam audio={false} height={200} ref={webcamRef} screenshotFormat="image/png" width={500} />
-				<canvas id="overlay" />
-				<button onClick={capture}>Capture photo</button>
-				<Form />
-				<img src={imgSrc} alt="capture" id="capture" crossOrigin="anonymous" />
-			</VStack>
-		</div>
-	);
+  return (
+    <div className="container">
+      <VStack className="column">
+        <Header />
+        <InfoBox />
+        <Webcam
+          audio={false}
+          height={200}
+          ref={webcamRef}
+          screenshotFormat="image/png"
+          width={500}
+        />
+        <canvas id="overlay" />
+        <button onClick={capture}>Capture photo</button>
+        <Form />
+        <img src={imgSrc} alt="capture" id="capture" crossOrigin="anonymous" />
+      </VStack>
+    </div>
+  );
 }
 
 function ConnectedApp() {
-	return (
-		<ChakraProvider>
-			<PageWrapper>
-				<App />
-			</PageWrapper>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider>
+      <PageWrapper>
+        <App />
+      </PageWrapper>
+    </ChakraProvider>
+  );
 }
 
 export default ConnectedApp;
