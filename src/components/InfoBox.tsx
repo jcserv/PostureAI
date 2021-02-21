c519e210acac26e3d978ffead8bc4550ca98f409
Jarrod Servilla <jarrod.servilla@gmail.com>
2021-02-20 23:54:13 -0500

    change desc of infobox

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -12,9 +12,9 @@ export const InfoBox = (): JSX.Element => {
         analyzes your posture & notifies you in real-time!
       </Text>
       <Text opacity={0.8} fontSize={{ base: "lg", lg: "xl" }} mt="6">
-        Get into an ergonomic position, click calibrate, set your timer, and get
-        cracking! PostureAI will notify you if your posture changes negatively,
-        allowing you to correct it quickly.
+        Get into an ergonomic position, set your timer, click calibrate, and get
+        cracking! You can minimize the browser and PostureAI will play an audio
+        notification if we identify bad posture!
       </Text>
       <ErgonomicGuidelines />
     </VStack>
